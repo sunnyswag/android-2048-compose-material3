12217a4a123aeafc156bd0bb6b991684be610b54
8cAyqpVKio <116042272+8cAyqpVKio@users.noreply.github.com>
2022-12-28 18:25:22 +0900

    refactor: separate GameUI into GameScreen

--- app/src/main/java/com/alexjlockwood/twentyfortyeight/ui/GameScreen.kt
+++ app/src/main/java/com/alexjlockwood/twentyfortyeight/ui/GameScreen.kt
@@ -1,25 +1,86 @@
 package com.alexjlockwood.twentyfortyeight.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.alexjlockwood.twentyfortyeight.R
 import com.alexjlockwood.twentyfortyeight.viewmodel.GameViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GameScreen(
     gameViewModel: GameViewModel = hiltViewModel()
 ) {
     AppTheme {
         Surface {
-            GameUi(
-                gridTileMovements = gameViewModel.gridTileMovements,
-                currentScore = gameViewModel.currentScore,
-                bestScore = gameViewModel.bestScore,
-                moveCount = gameViewModel.moveCount,
-                isGameOver = gameViewModel.isGameOver,
-                onNewGameRequested = { gameViewModel.startNewGame() },
-                onSwipeListener = { direction -> gameViewModel.move(direction) },
-            )
+            var shouldShowNewGameDialog by remember { mutableStateOf(false) }
+            Scaffold(
+                topBar = {
+                    GameTopAppBar(
+                        title = stringResource(R.string.app_name),
+                        onAddButtonClick = { shouldShowNewGameDialog = true }
+                    )
+                }
+            ) { innerPadding ->
+                GameUi(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize(),
+                    gridTileMovements = gameViewModel.gridTileMovements,
+                    currentScore = gameViewModel.currentScore,
+                    bestScore = gameViewModel.bestScore,
+                    moveCount = gameViewModel.moveCount,
+                    onSwipeListener = { direction -> gameViewModel.move(direction) },
+                )
+            }
+
+            if (gameViewModel.isGameOver) {
+                GameDialog(
+                    title = stringResource(R.string.msg_game_over),
+                    message = stringResource(R.string.msg_game_over_body),
+                    onConfirmListener = { gameViewModel.startNewGame() },
+                    onDismissListener = {
+                        // TODO: allow user to dismiss the dialog so they can take a screenshot
+                    },
+                )
+            }
+            else if (shouldShowNewGameDialog) {
+                GameDialog(
+                    title = stringResource(R.string.msg_start_new_game),
+                    message = stringResource(R.string.msg_start_new_game_body),
+                    onConfirmListener = {
+                        gameViewModel.startNewGame()
+                        shouldShowNewGameDialog = false
+                    },
+                    onDismissListener = {
+                        shouldShowNewGameDialog = false
+                    },
+                )
+            }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GameTopAppBar(
+    modifier: Modifier = Modifier,
+    title: String,
+    onAddButtonClick: () -> Unit
+) {
+    TopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        actions = {
+            IconButton(onClick = { onAddButtonClick() }) {
+                Icon(Icons.Filled.Add, contentDescription = "")
+            }
+        }
+    )
 }
--- app/src/main/java/com/alexjlockwood/twentyfortyeight/ui/GameUi.kt
+++ app/src/main/java/com/alexjlockwood/twentyfortyeight/ui/GameUi.kt
@@ -3,28 +3,24 @@ package com.alexjlockwood.twentyfortyeight.ui
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
+import com.alexjlockwood.twentyfortyeight.R
 import com.alexjlockwood.twentyfortyeight.domain.Direction
 import com.alexjlockwood.twentyfortyeight.domain.GridTileMovement
-import com.alexjlockwood.twentyfortyeight.R
+import java.lang.Math.toDegrees
+import kotlin.math.atan2
 
 /**
  * Renders the 2048 game's home screen UI.
  */
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GameUi(
     modifier: Modifier = Modifier,
@@ -32,162 +28,129 @@ fun GameUi(
     currentScore: Int,
     bestScore: Int,
     moveCount: Int,
-    isGameOver: Boolean,
-    onNewGameRequested: () -> Unit,
     onSwipeListener: (direction: Direction) -> Unit,
 ) {
     var swipeAngle by remember { mutableStateOf(0f) }
-    var shouldShowNewGameDialog by remember { mutableStateOf(false) }
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(stringResource(R.string.app_name)) },
-                modifier = modifier,
-                actions = {
-                    IconButton(onClick = { shouldShowNewGameDialog = true }) {
-                        Icon(Icons.Filled.Add, contentDescription = "")
+    BoxWithConstraints(
+        modifier = modifier
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDrag = { change, dragAmount ->
+                        change.consume()
+                        swipeAngle = uatan2(dragAmount.x, -(dragAmount.y))
+                    },
+                    onDragEnd = {
+                        onSwipeListener(
+                            when {
+                                45 <= swipeAngle && swipeAngle < 135 -> Direction.NORTH
+                                135 <= swipeAngle && swipeAngle < 225 -> Direction.WEST
+                                225 <= swipeAngle && swipeAngle < 315 -> Direction.SOUTH
+                                else -> Direction.EAST
+                            }
+                        )
                     }
-                }
+                )
+            }
+    ) {
+        val isPortrait = maxWidth < maxHeight
+        ConstraintLayout {
+            val (gameGrid, currentScoreText, currentScoreLabel,
+                bestScoreText, bestScoreLabel) = createRefs()
+            GameGrid(
+                modifier = Modifier
+                    .aspectRatio(1f)
+                    .padding(16.dp)
+                    .constrainAs(gameGrid) {
+                        if (isPortrait) {
+                            start.linkTo(parent.start)
+                            top.linkTo(parent.top)
+                            end.linkTo(parent.end)
+                        }
+                        else {
+                            start.linkTo(parent.start)
+                            top.linkTo(parent.top)
+                            bottom.linkTo(parent.bottom)
+                        }
+                    },
+                gridTileMovements = gridTileMovements,
+                moveCount = moveCount,
+            )
+            TextLabel(
+                modifier = Modifier
+                    .constrainAs(currentScoreText) {
+                        if (isPortrait) {
+                            start.linkTo(gameGrid.start, 16.dp)
+                            top.linkTo(gameGrid.bottom)
+                        }
+                        else {
+                            start.linkTo(currentScoreLabel.start)
+                            bottom.linkTo(currentScoreLabel.top)
+                        }
+                    },
+                text = "$currentScore",
+                fontSize = 36.sp
+            )
+            TextLabel(
+                modifier = Modifier
+                    .constrainAs(currentScoreLabel) {
+                        if (isPortrait) {
+                            start.linkTo(currentScoreText.start)
+                            top.linkTo(currentScoreText.bottom)
+                        }
+                        else {
+                            start.linkTo(bestScoreText.start)
+                            bottom.linkTo(bestScoreText.top)
+                        }
+                    },
+                text = stringResource(R.string.msg_score),
+                fontSize = 18.sp
+            )
+            TextLabel(
+                modifier = Modifier
+                    .constrainAs(bestScoreText) {
+                        if (isPortrait) {
+                            end.linkTo(gameGrid.end, 16.dp)
+                            top.linkTo(gameGrid.bottom)
+                        }
+                        else {
+                            start.linkTo(bestScoreLabel.start)
+                            bottom.linkTo(bestScoreLabel.top)
+                        }
+                    },
+                text = "$bestScore",
+                fontSize = 36.sp
+            )
+            TextLabel(
+                modifier = Modifier
+                    .constrainAs(bestScoreLabel) {
+                        if (isPortrait) {
+                            end.linkTo(bestScoreText.end)
+                            top.linkTo(bestScoreText.bottom)
+                        }
+                        else {
+                            start.linkTo(gameGrid.end)
+                            bottom.linkTo(gameGrid.bottom, 16.dp)
+                        }
+                    },
+                text = stringResource(R.string.msg_best_score),
+                fontSize = 18.sp
             )
         }
-    ) { innerPadding ->
-        BoxWithConstraints(
-            modifier = modifier
-                .padding(innerPadding)
-                .fillMaxSize()
-                .pointerInput(Unit) {
-                    detectDragGestures(
-                        onDrag = { change, dragAmount ->
-                            change.consume()
-                            swipeAngle = atan2(dragAmount.x, -(dragAmount.y))
-                        },
-                        onDragEnd = {
-                            onSwipeListener(
-                                when {
-                                    45 <= swipeAngle && swipeAngle < 135 -> Direction.NORTH
-                                    135 <= swipeAngle && swipeAngle < 225 -> Direction.WEST
-                                    225 <= swipeAngle && swipeAngle < 315 -> Direction.SOUTH
-                                    else -> Direction.EAST
-                                }
-                            )
-                        }
-                    )
-                }
-        ) {
-            val isPortrait = maxWidth < maxHeight
-            ConstraintLayout(
-                constraintSet = buildConstraints(isPortrait)
-            ) {
-                GameGrid(
-                    modifier = modifier
-                        .aspectRatio(1f)
-                        .padding(16.dp)
-                        .layoutId("gameGrid"),
-                    gridTileMovements = gridTileMovements,
-                    moveCount = moveCount,
-                )
-                TextLabel(text = "$currentScore", layoutId = "currentScoreText", fontSize = 36.sp)
-                TextLabel(text = "Score", layoutId = "currentScoreLabel", fontSize = 18.sp)
-                TextLabel(text = "$bestScore", layoutId = "bestScoreText", fontSize = 36.sp)
-                TextLabel(text = "Best", layoutId = "bestScoreLabel", fontSize = 18.sp)
-            }
-        }
-    }
-    if (isGameOver) {
-        GameDialog(
-            title = "Game over",
-            message = "Start a new game?",
-            onConfirmListener = { onNewGameRequested.invoke() },
-            onDismissListener = {
-                // TODO: allow user to dismiss the dialog so they can take a screenshot
-            },
-        )
-    } else if (shouldShowNewGameDialog) {
-        GameDialog(
-            title = "Start a new game?",
-            message = "Starting a new game will erase your current game",
-            onConfirmListener = {
-                onNewGameRequested.invoke()
-                shouldShowNewGameDialog = false
-            },
-            onDismissListener = {
-                shouldShowNewGameDialog = false
-            },
-        )
     }
 }
 
 @Composable
-private fun TextLabel(text: String, layoutId: String, fontSize: TextUnit) {
+private fun TextLabel(modifier: Modifier, text: String, fontSize: TextUnit) {
     Text(
         text = text,
-        modifier = Modifier.layoutId(layoutId),
+        modifier = modifier,
         fontSize = fontSize,
         fontWeight = FontWeight.Light,
     )
 }
 
-private fun buildConstraints(isPortrait: Boolean): ConstraintSet {
-    return ConstraintSet {
-        val gameGrid = createRefFor("gameGrid")
-        val currentScoreText = createRefFor("currentScoreText")
-        val currentScoreLabel = createRefFor("currentScoreLabel")
-        val bestScoreText = createRefFor("bestScoreText")
-        val bestScoreLabel = createRefFor("bestScoreLabel")
-
-        if (isPortrait) {
-            constrain(gameGrid) {
-                start.linkTo(parent.start)
-                top.linkTo(parent.top)
-                end.linkTo(parent.end)
-            }
-            constrain(currentScoreText) {
-                start.linkTo(gameGrid.start, 16.dp)
-                top.linkTo(gameGrid.bottom)
-            }
-            constrain(currentScoreLabel) {
-                start.linkTo(currentScoreText.start)
-                top.linkTo(currentScoreText.bottom)
-            }
-            constrain(bestScoreText) {
-                end.linkTo(gameGrid.end, 16.dp)
-                top.linkTo(gameGrid.bottom)
-            }
-            constrain(bestScoreLabel) {
-                end.linkTo(bestScoreText.end)
-                top.linkTo(bestScoreText.bottom)
-            }
-        } else {
-            constrain(gameGrid) {
-                start.linkTo(parent.start)
-                top.linkTo(parent.top)
-                bottom.linkTo(parent.bottom)
-            }
-            constrain(currentScoreText) {
-                start.linkTo(currentScoreLabel.start)
-                bottom.linkTo(currentScoreLabel.top)
-            }
-            constrain(currentScoreLabel) {
-                start.linkTo(bestScoreText.start)
-                bottom.linkTo(bestScoreText.top)
-            }
-            constrain(bestScoreText) {
-                start.linkTo(bestScoreLabel.start)
-                bottom.linkTo(bestScoreLabel.top)
-            }
-            constrain(bestScoreLabel) {
-                start.linkTo(gameGrid.end)
-                bottom.linkTo(gameGrid.bottom, 16.dp)
-            }
-            createHorizontalChain(gameGrid, bestScoreLabel, chainStyle = ChainStyle.Packed)
-        }
+private fun uatan2(x: Float, y: Float): Float =
+    toDegrees(atan2(y, x).toDouble()).toFloat().let { deg ->
+        if (deg < 0) { deg + 360 }
+        else deg
     }
-}
-
-private fun atan2(x: Float, y: Float): Float {
-    var degrees = Math.toDegrees(kotlin.math.atan2(y, x).toDouble()).toFloat()
-    if (degrees < 0) {
-        degrees += 360
-    }
-    return degrees
-}
